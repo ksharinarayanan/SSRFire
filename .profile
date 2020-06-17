7eef52e2b69f5371c326ac3ed561bd0fab1a346d
Harinarayanan K S <ksharinarayanan36@gmail.com>
2020-06-17 21:42:20 +0530

    Changed custom warnings

---
+++ .profile
@@ -0,0 +1,48 @@
+gau(){
+	tools/gau/./main $1
+}
+gau_s(){
+	tools/gau/./main --subs $1
+}
+
+ffuf(){
+	tools/ffuf/./main -u $1 -w $2 -b $3 -c -t 100 
+}
+openredirex(){
+	python3 tools/OpenRedireX/openredirex.py -l $1 -p $2
+}
+qsreplace(){
+	tools/qsreplace/./main $1
+}
+gau(){
+	tools/gau/./main $1
+}
+gau_s(){
+	tools/gau/./main --subs $1
+}
+
+ffuf(){
+	tools/ffuf/./main -u $1 -w $2 -b $3 -c -t 100 
+}
+openredirex(){
+	python3 tools/OpenRedireX/openredirex.py -l $1 -p $2
+}
+qsreplace(){
+	tools/qsreplace/./main $1
+}
+gau(){
+	tools/gau/./main $1
+}
+gau_s(){
+	tools/gau/./main --subs $1
+}
+
+ffuf(){
+	tools/ffuf/./main -u $1 -w $2 -b $3 -c -t 100 
+}
+openredirex(){
+	python3 tools/OpenRedireX/openredirex.py -l $1 -p $2
+}
+qsreplace(){
+	tools/qsreplace/./main $1
+}
--- setup.sh
+++ setup.sh
@@ -9,78 +9,59 @@ if [ ! -d output ]; then
 	mkdir output
 fi
 echo "${red}Note: You must have go and python 3.7 installed for the tools to work${reset}"
-read -p "${yellow}Do you want to install gau[y/n]:${reset} " input
-if [[ $input == 'y' ]]; then
-	echo "${cyan}Installing gau... ${reset}"
-	mkdir tools
-	cd tools
-	git clone https://github.com/lc/gau.git
-	echo "Building the main.go file. ${red}This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
-       	cd gau
-	go build main.go
-	chmod +x main
-	cd ../..
-	echo -e "gau(){
-		tools/gau/./main \$1
-	}
-	gau_s(){
-		tools/gau/./main --subs \$1
-	}
-	" >> .profile
-	echo -e "\n"
-fi
+echo "${cyan}Installing gau... ${reset}"
+mkdir tools
+cd tools
+git clone https://github.com/lc/gau.git
+echo -e "Building the main.go file.\n${yellow}[Warning] This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
+cd gau
+go build main.go
+chmod +x main
+cd ../..
+echo -e "gau(){
+	tools/gau/./main \$1
+}
+gau_s(){
+	tools/gau/./main --subs \$1
+}
+" >> .profile
+echo -e "\n"
 
-read -p "${yellow}Do you want to install ffuf[y/n]: ${reset}" input
-if [[ $input == 'y' ]]; then
-	echo "${cyan}Installing ffuf... ${reset}"
-	if [ ! -d tools ]; then
-		mkdir tools
-	fi
-	cd tools
-	git clone https://github.com/ffuf/ffuf.git
-	echo "Building the main.go file. ${red}This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
-	cd ffuf
-	go build main.go help.go
-	chmod +x main
-	cd ../..
-	echo -e "ffuf(){
-		tools/ffuf/./main -u \$1 -w \$2 -b \$3 -c -t 100 
-	}" >> .profile
-	echo -e "\n"
-fi
+echo "${cyan}Installing ffuf... ${reset}"
+cd tools
+git clone https://github.com/ffuf/ffuf.git
+echo -e "Building the main.go file.\n${yellow}[Warning] This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
+cd ffuf
+go build main.go help.go
+chmod +x main
+cd ../..
+echo -e "ffuf(){
+	tools/ffuf/./main -u \$1 -w \$2 -b \$3 -c -t 100 
+}" >> .profile
+echo -e "\n"
 
-read -p "${yellow}Do you want to install OpenRedireX[y/n]:  ${reset}" input
-if [[ $input == 'y' ]]; then
-	echo "${cyan}Installing OpenRedireX... ${reset}"
-	if [ ! -d tools ]; then
-		mkdir tools
-	fi
-	cd tools 
-	git clone https://github.com/devanshbatham/OpenRedireX.git
-	cd ..
-	echo -e "openredirex(){
-		python3 tools/OpenRedireX/openredirex.py -l \$1 -p \$2
-	}" >> .profile
-	echo -e "${green}Done${reset}\n"
-fi
 
-read -p "${yellow}Do want to install qsreplace[y/n]: ${reset}" input
-if [[ $input == 'y' ]]; then
-	echo "${cyan}Installing qsreplace...${reset}"
-	if [ ! -d tools ]; then
-		mkdir tools
-	fi
-	cd tools 
-	git clone https://github.com/tomnomnom/qsreplace
-	echo "Building the main.go file. ${red}This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
-	cd qsreplace
-	go build main.go
-	cd ../..
-	echo -e "qsreplace(){
-		tools/qsreplace/./main \$1
-	}" >> .profile
-	echo -e "\n"
-fi
+echo "${cyan}Installing OpenRedireX... ${reset}"
+cd tools 
+git clone https://github.com/devanshbatham/OpenRedireX.git
+cd ..
+echo -e "openredirex(){
+	python3 tools/OpenRedireX/openredirex.py -l \$1 -p \$2
+}" >> .profile
+echo -e "${green}Done${reset}\n"
+
+
+echo "${cyan}Installing qsreplace...${reset}"
+cd tools
+git clone https://github.com/tomnomnom/qsreplace
+echo -e "Building the main.go file.\n${yellow}[Warning] This process fails if you don't have go installed. If you have any error messages below, try again after installing go.${reset}"
+cd qsreplace
+go build main.go
+cd ../..
+echo -e "qsreplace(){
+	tools/qsreplace/./main \$1
+}" >> .profile
+echo -e "\n"
 if [ -d tools ]; then
 	source .profile
 	echo "Your tools are installed under the tools directory"
